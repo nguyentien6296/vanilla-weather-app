da95744a7bc158b3ef014cad4de1b2342753a367
nguyentien6296 <88523384+nguyentien6296@users.noreply.github.com>
2022-04-24 18:51:04 +0700

    add forecast template

--- src/app.js
+++ src/app.js
@@ -20,6 +20,38 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+       <div class="weather-forecast-date">${day}</div>
+       <img 
+          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+          alt="" />
+       <div class="weather-forecast-temperatures">
+         <span class="weather-forecast-temperature-max"> 18° </span>
+         <span class="weather-forecast-temperature-min"> 12° </span>
+       </div>
+       </div>
+`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "1c2d25d3b778335dacad0d1205b20d4e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -42,6 +74,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "1c2d25d3b778335dacad0d1205b20d4e";
@@ -73,6 +107,7 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
